c271fce514b5e9f2f8941d5178af58bf5c9931b4
PixelsCommander <denis.radin@gmail.com>
2015-11-13 15:23:49 +0100

    Changed port for IO

--- demo/server.js
+++ demo/server.js
@@ -31,7 +31,7 @@ app.get('/', function (req, res) {
 
 app.use(express.static('./'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
 
